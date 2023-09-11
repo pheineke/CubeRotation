2fc77e621bf584c10a688446a974cef1ac4e2d3d
rxR4GE <heineke.pe@gmail.com>
2023-09-11 14:05:53 +0200

    Cube Points Rotating

--- main2.py
+++ main2.py
@@ -19,39 +19,115 @@ pointsize = 4
 pointsizetup = (pointsize, pointsize)
 
 radius = 200
+a = 100  # horizontaler Halbparameter des Ovals
+b = 50  # vertikaler Halbparameter des Ovals
 rotationtime = 1
 
 
 recttemp = [[0, 0] for i in range(4)]
+recttemp1 = [[0, 0] for i in range(4)]
+# Calculate the points
+rect = [[0, 0] for i in range(4)]
+rect1 = [[0, 0] for i in range(4)]
 
 def draw(angle):
     color = (255,0,0)
+    color0 = (255, 0, 0)    # Rot
+    color1 = (0, 255, 0)    # Grün
+    color2 = (0, 0, 255)    # Blau
+    color3 = (255, 255, 0)  # Gelb
+    color4 = (255, 0, 255)  # Magenta
+    color5 = (0, 255, 255)  # Cyan
 
-
-    
-    
 
     angles = [angle_step * i for i in range(4)]
 
-    
+    #Erase old rectangle:
+
+    '''
     pygame.draw.rect(window, background, ((recttemp[0][0], recttemp[0][1]), pointsizetup))
     pygame.draw.rect(window, background, ((recttemp[1][0], recttemp[1][1]), pointsizetup))
     pygame.draw.rect(window, background, ((recttemp[2][0], recttemp[2][1]), pointsizetup))
     pygame.draw.rect(window, background, ((recttemp[3][0], recttemp[3][1]), pointsizetup))
+    '''
+    #>
+    for i in range(0,4):
+        pygame.draw.rect(window, background, ((recttemp[i][0], recttemp[i][1]), pointsizetup))
 
-    # Calculate the points
-    rect = [[0, 0] for i in range(4)]
-    for i in range(4):
-        rect[i][0] = center[0] + radius * math.cos(angle + angles[i])
-        rect[i][1] = center[1] + radius * math.sin(angle + angles[i])
-        recttemp[i][0] = center[0] + radius * math.cos(angle + angles[i])
-        recttemp[i][1] = center[1] + radius * math.sin(angle + angles[i])
+    '''
+    pygame.draw.rect(window, background, ((recttemp1[0][0], recttemp1[0][1]), pointsizetup))
+    pygame.draw.rect(window, background, ((recttemp1[1][0], recttemp1[1][1]), pointsizetup))
+    pygame.draw.rect(window, background, ((recttemp1[2][0], recttemp1[2][1]), pointsizetup))
+    pygame.draw.rect(window, background, ((recttemp1[3][0], recttemp1[3][1]), pointsizetup))
+    '''
+    #>
+    for i in range(0,4):
+        pygame.draw.rect(window, background, ((recttemp1[i][0], recttemp1[i][1]), pointsizetup))
 
     
+    for i in range(0,4):
+        for j in range(0,4):
+            pygame.draw.line(window, background, (rect[i][0], rect[i][1]), (rect[j][0], rect[j][1]), 1)
+
+    ####
+
+    
+
+    for i in range(4):
+    # Berechnung der x- und y-Koordinaten
+        x = center[0] + a * math.cos(angle + angles[i]) 
+        y = center[1] + b * math.sin(angle + angles[i])
+        
+        # Aktualisierung der Positionen
+        rect[i][0] = x
+        rect[i][1] = y
+        recttemp[i][0] = x
+        recttemp[i][1] = y
+
+    for i in range(4):
+    # Berechnung der x- und y-Koordinaten
+        x = center[0] + a * math.cos(angle + angles[i]) 
+        y = center[1] + b * math.sin(angle + angles[i]) - 100
+        
+        # Aktualisierung der Positionen
+        rect1[i][0] = x
+        rect1[i][1] = y
+        recttemp1[i][0] = x
+        recttemp1[i][1] = y
+
+
+    #Draw New Rectangle:
+
+    '''
     pygame.draw.rect(window, color, ((rect[0][0], rect[0][1]), pointsizetup))
     pygame.draw.rect(window, color, ((rect[1][0], rect[1][1]), pointsizetup))
     pygame.draw.rect(window, color, ((rect[2][0], rect[2][1]), pointsizetup))
     pygame.draw.rect(window, color, ((rect[3][0], rect[3][1]), pointsizetup))
+    '''
+    #>
+    for i in range(0,4):
+        pygame.draw.rect(window, color, ((rect[i][0], rect[i][1]), pointsizetup))
+
+    '''
+    pygame.draw.rect(window, color, ((rect1[0][0], rect1[0][1]), pointsizetup))
+    pygame.draw.rect(window, color, ((rect1[1][0], rect1[1][1]), pointsizetup))
+    pygame.draw.rect(window, color, ((rect1[2][0], rect1[2][1]), pointsizetup))
+    pygame.draw.rect(window, color, ((rect1[3][0], rect1[3][1]), pointsizetup))
+    '''
+    #>
+    for i in range(0,4):
+        pygame.draw.rect(window, color, ((rect1[i][0], rect1[i][1]), pointsizetup))
+
+
+    for i in range(0,4):
+        for j in range(0,4):
+                pygame.draw.line(window, color0, (rect[i][0], rect[i][1]), (rect[j][0], rect[j][1]), 1)
+                pygame.draw.line(window, color1, (rect[(i+1)%5][0], rect[(i+1)%5][1]), (rect[(j+1)%5][0], rect[(j+1)%5][1]), 1)
+
+
+
+
+    ####
 
     pygame.display.update()
 
@@ -71,7 +147,8 @@ center = (window_size[0] // 2, window_size[1] // 2)
 
 running = True
 while running:
-    for event in pygame.event.get():
+    x = pygame.event.get()
+    for event in x:
         if event.type == pygame.QUIT:
             running = False
 
---
+++ test.py
@@ -0,0 +1,7 @@
+import time
+
+#Modulo Tests
+for i in range(0,5):
+    print((i+1)%5)
+    time.sleep(4)
+    print(5%5)
